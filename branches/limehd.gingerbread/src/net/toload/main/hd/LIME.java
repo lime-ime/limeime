0ec34eb50579f9b47f1e7421fb1699136993a245
jrywjwu <jrywjwu@256c4b9c-cab3-11de-a9c8-2fbcfba63733>
2011-08-02 18:30:15 +0000

    Dirty implementation of gingerbread LatinKeyboardBaseVIEW derivation with multi-touch capability

---
+++ branches/limehd.gingerbread/src/net/toload/main/hd/LIME.java
@@ -0,0 +1,62 @@
+package net.toload.main.hd;
+
+public class LIME {
+	
+	protected static final String DAYI_DOWNLOAD_URL = "http://openvanilla.googlecode.com/svn/trunk/Modules/SharedData/dayi3.cin";
+	protected static final String CJ_DOWNLOAD_URL = "http://www.openfoundry.org/websvn/filedetails.php?repname=limeime&path=%2Fdatabases%2Flimehd%2Fcj.zip";
+	protected static final String SCJ_DOWNLOAD_URL = "http://www.openfoundry.org/websvn/filedetails.php?repname=limeime&path=%2Fdatabases%2Flimehd%2Fscj.zip";
+	protected static final String ARRAY_DOWNLOAD_URL = "http://www.openfoundry.org/websvn/filedetails.php?repname=limeime&path=%2Fdatabases%2Flimehd%2Farray.zip";
+	protected static final String ARRAY10_DOWNLOAD_URL = "http://www.openfoundry.org/websvn/filedetails.php?repname=limeime&path=%2Fdatabases%2Flimehd%2Farray10.zip";
+	protected static final String EZ_DOWNLOAD_URL = "http://www.openfoundry.org/websvn/filedetails.php?repname=limeime&path=%2Fdatabases%2Flimehd%2Fez.zip";
+	protected static final String PHONETIC_DOWNLOAD_URL = "http://www.openfoundry.org/websvn/filedetails.php?repname=limeime&path=%2Fdatabases%2Flimehd%2Fphonetic.zip";
+	protected static final String PHONETICADV_DOWNLOAD_URL = "http://www.openfoundry.org/websvn/filedetails.php?repname=limeime&path=%2Fdatabases%2Flimehd%2Fphonetic_adv.zip";
+	protected static final String CJ5_DOWNLOAD_URL = "http://www.openfoundry.org/websvn/filedetails.php?repname=limeime&path=%2Fdatabases%2Flimehd%2Fcj5.zip";
+	protected static final String ECJ_DOWNLOAD_URL = "http://www.openfoundry.org/websvn/filedetails.php?repname=limeime&path=%2Fdatabases%2Flimehd%2Fecj.zip";
+	protected static final String WB_DOWNLOAD_URL = "http://www.openfoundry.org/websvn/filedetails.php?repname=limeime&path=%2Fdatabases%2Flimehd%2Fwb.zip";
+	protected static final String IM_DOWNLOAD_TARGET_PRELOADED = "http://www.openfoundry.org/websvn/filedetails.php?repname=limeime&path=%2Fdatabases%2Flimehd%2Flime1107.zip";
+	protected static final String IM_DOWNLOAD_TARGET_EMPTY = "http://www.openfoundry.org/websvn/filedetails.php?repname=limeime&path=%2Fdatabases%2Flimehd%2Fempty1107.zip";
+	//protected static final String IM_DOWNLOAD_TARGET_PRELOADED = "http://limeime.googlecode.com/svn/branches/database/lime1104.zip";
+	//protected static final String IM_DOWNLOAD_TARGET_EMPTY = "http://limeime.googlecode.com/svn/branches/database/empty1104.zip";
+	protected static final String IM_LOAD_LIME_ROOT_DIRECTORY = "/sdcard/limehd/";
+	protected static final String DOWNLOAD_START = "download_start";
+	protected static final String DATABASE_DOWNLOAD_STATUS = "database_download_status";
+	protected static final String DATABASE_NAME = "lime.db";
+	protected static final String DATABASE_SOURCE_DAYI = "dayi.cin";
+	protected static final String DATABASE_SOURCE_PHONETIC = "phonetic.lime";
+	protected static final String DATABASE_SOURCE_PHONETICADV = "phonetic_adv.lime";
+	protected static final String DATABASE_SOURCE_CJ = "cj.lime";
+	protected static final String DATABASE_SOURCE_CJ5 = "cj5.lime";
+	protected static final String DATABASE_SOURCE_ECJ = "ecj.lime";
+	protected static final String DATABASE_SOURCE_SCJ = "scj.lime";
+	protected static final String DATABASE_SOURCE_ARRAY = "array.lime";
+	protected static final String DATABASE_SOURCE_ARRAY10 = "array10.lime";
+	protected static final String DATABASE_SOURCE_WB = "wb.lime";
+	protected static final String DATABASE_SOURCE_EZ = "ez.lime";
+	protected static final String DATABASE_SOURCE_FILENAME = "lime.zip";
+	protected static final String DATABASE_SOURCE_FILENAME_EMPTY = "empty.zip";
+	protected static final String DATABASE_DECOMPRESS_FOLDER = "/data/data/net.toload.main.hd/databases";
+	protected static final String DATABASE_DECOMPRESS_FOLDER_SDCARD = "/sdcard/limehd/databases";
+	protected static final String DATABASE_BACKUP_NAME = "backup.zip";
+	protected static final String IM_CJ_STATUS = "im_cj_status";
+	protected static final String IM_SCJ_STATUS = "im_scj_status";
+	protected static final String IM_PHONETIC_STATUS = "im_phonetic_status";
+	protected static final String IM_DAYI_STATUS = "im_dayi_status";
+	protected static final String IM_CUSTOM_STATUS = "im_custom_status";
+	protected static final String IM_EZ_STATUS = "im_ez_status";
+	
+	protected static final String IM_MAPPING_FILENAME = "im_mapping_filename";
+	protected static final String IM_MAPPING_VERSION = "im_mapping_version";
+	protected static final String IM_MAPPING_TOTAL = "im_mapping_total";
+	protected static final String IM_MAPPING_DATE = "im_mapping_date";
+
+	protected static final String CANDIDATE_SUGGESTION = "candidate_suggestion";
+	protected static final String TOTAL_USERDICT_RECORD = "total_userdict_record";
+	protected static final String LEARNING_SWITCH = "learning_switch";
+
+	
+
+
+	protected final static String SEARCHSRV_RESET_CACHE = "searchsrv_reset_cache";
+	protected final static int SEARCHSRV_RESET_CACHE_SIZE = 500;
+	
+}
