4b95432ad7eb053799c54a85b4982b218ded9894
jrywjwu <jrywjwu@256c4b9c-cab3-11de-a9c8-2fbcfba63733>
2011-08-06 12:38:03 +0000

    1. Modified LIMEKeyboardBaseView for compatibility to eclair(2.0/2.1) 2. Remove the left/right button on candidate view.

--- src/net/toload/main/hd/candidate/CandidateViewContainer.java
+++ src/net/toload/main/hd/candidate/CandidateViewContainer.java
@@ -24,7 +24,7 @@ public class CandidateViewContainer extends LinearLayout implements OnTouchListe
 
     public void initViews() {
         if (mCandidates == null) {
-            mButtonLeftLayout = findViewById(R.id.candidate_left_parent);
+           /* mButtonLeftLayout = findViewById(R.id.candidate_left_parent);
             mButtonLeft = findViewById(R.id.candidate_left);
             if (mButtonLeft != null) {
                 mButtonLeft.setOnTouchListener(this);
@@ -33,14 +33,14 @@ public class CandidateViewContainer extends LinearLayout implements OnTouchListe
             mButtonRight = findViewById(R.id.candidate_right);
             if (mButtonRight != null) {
                 mButtonRight.setOnTouchListener(this);
-            }
+            }*/
             mCandidates = (CandidateView) findViewById(R.id.candidates);
         }
     }
 
     @Override
     public void requestLayout() {
-        if (mCandidates != null) {
+/*        if (mCandidates != null) {
             int availableWidth = mCandidates.getWidth();
             int neededWidth = mCandidates.computeHorizontalScrollRange();
             int x = mCandidates.getScrollX();
@@ -52,18 +52,18 @@ public class CandidateViewContainer extends LinearLayout implements OnTouchListe
             if (mButtonRightLayout != null) {
                 mButtonRightLayout.setVisibility(rightVisible ? VISIBLE : GONE);
             }
-        }
+        }*/
         super.requestLayout();
     }
 
     public boolean onTouch(View v, MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+        /*if (event.getAction() == MotionEvent.ACTION_DOWN) {
             if (v == mButtonRight) {
                 mCandidates.scrollNext();
             } else if (v == mButtonLeft) {
                 mCandidates.scrollPrev();
             }
-        }
+        }*/
         return false;
     }
     
