0190bc58674a6c618a503ff35e6720c5bd19d053
jrywjwu <jrywjwu@256c4b9c-cab3-11de-a9c8-2fbcfba63733>
2011-08-20 14:27:56 +0000

    New LIMEService unit test.

---
+++ branches/limehd.gingerbread/tests/src/net/toload/main/hd/test/TestLimeService.java
@@ -0,0 +1,136 @@
+package net.toload.main.hd.test;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Random;
+
+import net.toload.main.hd.LIMEService;
+import android.content.Intent;
+import android.os.Debug;
+import android.os.SystemClock;
+import android.test.ServiceTestCase;
+import android.util.Log;
+import android.view.inputmethod.EditorInfo;
+
+public class TestLimeService extends ServiceTestCase<LIMEService> {
+	
+	private final String TAG = "TestLimeService";
+	private final static String TEST_KEY = "1234567890qwertyuiopasdfghjkl;zxcvbnm,./-";
+	private List<String> keyList;
+
+	public TestLimeService() {
+		super(LIMEService.class);
+	}
+	protected void setUp() throws Exception {
+		super.setUp();
+		mContext = getContext();
+
+	}
+
+	protected void tearDown() throws Exception {
+		mContext = null;
+		super.tearDown();
+	}
+	
+	 public void testLIMEServiceStart() {
+	        Log.i(TAG, "testLIMEServiceStart() starting...");
+	        try {
+	            Intent intent = new Intent();
+	            startService(intent);
+	            LIMEService Serv=getService();
+	            assertNotNull(Serv);
+	            
+	            Serv.onCreateCandidatesView();
+	            Serv.onCreateInputView();
+	            
+	            EditorInfo attribute = new EditorInfo();
+	            attribute.inputType = EditorInfo.TYPE_CLASS_TEXT;
+				Serv.onStartInput(attribute, false);
+				
+				randomTypingTest(Serv);
+				
+
+	         } catch (Exception e) {
+	            e.printStackTrace();
+	            fail(e.getMessage());
+	        } finally {
+	            Log.i(TAG, "testLIMEServiceStart() ended.");
+	        }
+	  }
+	private void simulateInput(LIMEService Serv, String code) {
+		
+		int[] keyCodes = {0};
+		char[] charray = code.toCharArray();
+		for(char key: charray){
+			long begin =  System.currentTimeMillis();
+			Serv.onKey((int)key, keyCodes);
+			long elapsed =  System.currentTimeMillis() - begin;
+			SystemClock.sleep(10);
+			Log.i(TAG,"Simulate onkey() with key=" + String.valueOf(key) + ", using " + elapsed + " ms");
+		}
+		if(Serv.hasMappingList) 
+			//Log.i(TAG, "testLIMEServiceStart() hasMappingList true");
+			Serv.pickDefaultCandidate();
+	}
+	
+	private void randomTypingTest(LIMEService Serv){
+		buildKeyMap();
+		
+		//Testing parameters.---------------
+		int limit = 1000;   //times performing random queries
+		//long timelimit = 250;// Assert the query time smaller than this time spec.
+		Debug.startMethodTracing("LimeService");
+
+		HashSet<String> duplityCheck = new HashSet<String>();
+		Random randomGenerator = new Random();
+		int count=0;
+		while(count<limit){
+			int size = randomGenerator.nextInt(4);
+			String code ="";
+			for(int i=0; i<size; i++){
+				code +=	keyList.get(randomGenerator.nextInt(keyList.size()));
+			}
+			
+			if(duplityCheck.add(code)){
+				Log.i(TAG,"Simulate typing code=" + code + ", count=" + count);
+				simulateInput(Serv, code);
+			}
+			SystemClock.sleep(100);
+			count++;
+		}
+		Debug.stopMethodTracing();
+
+	}
+	 public void testLIMEServiceStop() {
+		 Log.i(TAG, "testLIMEServiceStop() starting...");
+
+	        try {
+	            Intent intent = new Intent();
+
+	            startService(intent);
+	            LIMEService service = getService();
+
+	            service.stopService(intent);
+	        }
+	        catch (Exception e) {
+	            e.printStackTrace();
+	            fail(e.toString());
+	        }
+	        finally {
+	            Log.i(TAG, "testLIMEServiceStop() ended");
+	        }
+	    }
+	 
+	private void buildKeyMap(){
+		 String keyString = TEST_KEY;
+		 keyList = new ArrayList<String>();
+		 for (int i = 0; i < keyString.length(); i++) {
+			 keyList.add( keyString.substring(i, i + 1));
+			
+		}
+	 }
+	 
+
+}
