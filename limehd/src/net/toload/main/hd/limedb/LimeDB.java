0c95eae68e7d089fa08712ba7460e32523219a94
hosoyu@gmail.com <hosoyu@gmail.com@256c4b9c-cab3-11de-a9c8-2fbcfba63733>
2011-09-30 15:00:26 +0000

    1. few minor bug fixed for stroke5, 2. adjust the message to be display when database failed to open, 3.  add maximum input limitation for for stroke5 ( <=5 composing text)

--- limehd/src/net/toload/main/hd/limedb/LimeDB.java
+++ limehd/src/net/toload/main/hd/limedb/LimeDB.java
@@ -692,7 +692,7 @@ public class LimeDB extends SQLiteOpenHelper {
 			
 		}catch(Exception e){
 			e.printStackTrace();
-			Toast.makeText(ctx, ctx.getText(R.string.l3_initial_download_failed), Toast.LENGTH_SHORT).show();
+			Toast.makeText(ctx, ctx.getText(R.string.l3_initial_database_failed), Toast.LENGTH_SHORT).show();
 			return null;
 		}
 		
